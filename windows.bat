c9507e31e7370244277383dc7c84b7305a73b1bc
Fullchee Zhang <Fullchee@users.noreply.github.com>
2017-10-04 23:14:08 -0400

    add meld

--- windows.bat
+++ windows.bat
@@ -1,2 +1,2 @@
 rem install choco first
-choco install flashplayerplugin googlechrome adobereader jre8 firefox 7zip.install notepadpluplus.install vlc nodejs.install git.install keepass.install visualstudiocode paint.net cmder youtube-dl python sublimetext3 googledrive pip f.lux
+choco install flashplayerplugin googlechrome adobereader jre8 firefox 7zip.install meld notepadpluplus.install vlc nodejs.install git.install keepass.install visualstudiocode paint.net cmder youtube-dl python sublimetext3 googledrive pip f.lux
