acad8fff6670cf5d17eb309926a423b3fd18f509
Fullchee Zhang <Fullchee@users.noreply.github.com>
2017-10-04 23:18:13 -0400

    alphabetically sorted the installed program
    easily check if I already added a file

--- windows.bat
+++ windows.bat
@@ -1,2 +1,13 @@
 rem install choco first
-choco install flashplayerplugin googlechrome adobereader jre8 firefox 7zip.install meld notepadpluplus.install vlc nodejs.install git.install keepass.install visualstudiocode paint.net cmder youtube-dl python sublimetext3 googledrive pip f.lux
+choco install cmder ^
+f.lux ^
+git.install ^
+googledrive ^
+keepass.install ^
+paint.net ^
+pip ^
+python ^
+sublimetext3 ^
+visualstudiocode ^
+youtube-dl ^
+
