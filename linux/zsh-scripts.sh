14f50fe75fd8020abebbb5a81426a5072f676c51
Fullchee Zhang <Fullchee@users.noreply.github.com>
2018-12-16 23:57:59 -0500

    Update zsh-scripts.sh

--- linux/zsh-scripts.sh
+++ linux/zsh-scripts.sh
@@ -2,9 +2,13 @@
 chsh -s /bin/zsh
 
 # install prezto
+cd ~
+rm -rf ~/.zprezto
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 setopt EXTENDED_GLOB
 
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
+
+config reset --hard
