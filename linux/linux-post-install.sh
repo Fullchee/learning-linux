790cb7b0b2184aaed53fdefd2c46307971655c2f
Fullchee Zhang <Fullchee@users.noreply.github.com>
2019-04-13 23:07:00 -0400

    Add youtubedl

--- linux/linux-post-install.sh
+++ linux/linux-post-install.sh
@@ -89,6 +89,7 @@ sudo apt-get -y install apt-transport-https # for vscode
 sudo apt-get -y install code
 sudo apt-get -y install sqlitebrowser
 sudo apt-get -y install okular
+sudo apt-get -y install youtube-dl
 
 sudo apt remove -y tomboy
 sudo apt-get remove -y gnome-screenshot # I use shutter instead, you can edit 
