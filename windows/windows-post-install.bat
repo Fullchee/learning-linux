e41b3d94f71a1092284b3f257573f6ad7c40370e
Fullchee Zhang <Fullchee@users.noreply.github.com>
2019-06-28 15:26:01 -0400

    Add installation of Choco

--- windows/windows-post-install.bat
+++ windows/windows-post-install.bat
@@ -1,4 +1,7 @@
 rem install choco first
+
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+
 choco install cmder ^
 f.lux ^
 git.install ^
