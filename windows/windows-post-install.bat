97d737c65c81dd14cbbf6cd1f85a36d943b56928
Fullchee Zhang <Fullchee@users.noreply.github.com>
2019-07-09 17:38:30 -0400

    Add dotfiles to windows post install script

--- windows/windows-post-install.bat
+++ windows/windows-post-install.bat
@@ -1,5 +1,4 @@
 rem install choco first
-
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
 choco install cmder ^
@@ -20,3 +19,10 @@ rem TODO: startup files
 rem shell:common startup in Run (cmd r)
 rem C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp
 
+rem TODO: after installing cmder
+
+# install my dotfiles after installing cmder (and git!)
+git clone --bare https://github.com/Fullchee/windows-dotfiles.git C:\.cfg
+alias config=git --git-dir=C:\.cfg --work-tree=C:\ $*
+config config --local status.showUntrackedFiles no
+config reset --hard
